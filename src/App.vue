ce2f7d74c0444b2c7474b0ff96edb27fd9f638d6
decent <danielekwuazi@gmail.com>
2017-08-27 23:11:59 +0700

    response

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,10 @@ export default {
   },
   methods: {
     translateText(text) {
-      this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170824T042513Z.ee6ff7e10f61b641.6001d15dc0e2ce6bc258eb80692dbb5e4b5d096d');
+      this.$http.get("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170824T042513Z.ee6ff7e10f61b641.6001d15dc0e2ce6bc258eb80692dbb5e4b5d096d&text=`text`&lang=ru")
+      .then((response) => {
+        console.log(response)
+      })
     },
   },
 };
