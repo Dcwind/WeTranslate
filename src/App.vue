bbbf632ed121c257dcf77a470ba8be6f52f15c9c
decent <danielekwuazi@gmail.com>
2017-08-27 20:04:37 +0700

    request to yandex translate api in progress

--- src/App.vue
+++ src/App.vue
@@ -14,10 +14,10 @@ export default {
     TranslationForm,
   },
   methods: {
-    translateText() {
-      
-    }
-  }
+    translateText(text) {
+      this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170824T042513Z.ee6ff7e10f61b641.6001d15dc0e2ce6bc258eb80692dbb5e4b5d096d');
+    },
+  },
 };
 </script>
 
