9f50b013e8ae4bd417e1b1e60c144affe2a743c1
decent <danielekwuazi@gmail.com>
2017-08-28 12:07:44 +0700

    language options

--- src/components/TranslationForm.vue
+++ src/components/TranslationForm.vue
@@ -3,6 +3,12 @@
     <p>translationForm component</p>
     <form @submit.prevent="formSubmit">
       <input type="text" v-model="textToTranslate" placeholder="Enter text to be translated">
+      <select v-model="lang">
+        <option value="ru">Russian</option>
+        <option value="fr">French</option>
+        <option value="zh">Chinese</option>
+        <option value="es">Spanish</option>
+      </select>
       <button type="submit" >Translate</button>
     </form>
   </div>
@@ -14,6 +20,7 @@ export default {
   data() {
     return {
       textToTranslate: '',
+      lang: '',
     };
   },
   methods: {
