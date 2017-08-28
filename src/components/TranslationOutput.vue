3d87bef77ea915b2ee60fd44c6d7d41903740828
decent <danielekwuazi@gmail.com>
2017-08-28 11:57:01 +0700

    props

--- src/components/TranslationOutput.vue
+++ src/components/TranslationOutput.vue
@@ -1,25 +1,12 @@
 <template>
   <div id="translationOut">
-    <p>translationOut component</p>
-    <form @submit.prevent="formSubmit">
-      <input type="text" v-model="textToTranslate" placeholder="Enter text to be translated">
-      <button type="submit" >Translate</button>
-    </form>
+    <p>{{translatedText}}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'translationOut',
-  data() {
-    return {
-      textToTranslate: '',
-    };
-  },
-  methods: {
-    formSubmit() {
-      this.$emit('formSubmit', this.textToTranslate);
-    },
-  },
+  props: ['translatedText'],
 };
 </script>
