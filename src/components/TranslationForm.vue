e4f500c106a2e8b674cff0f2a5540f24312f68a8
decent <danielekwuazi@gmail.com>
2017-08-29 10:24:15 +0700

    passing selected language up to App

--- src/components/TranslationForm.vue
+++ src/components/TranslationForm.vue
@@ -3,7 +3,7 @@
     <p>translationForm component</p>
     <form @submit.prevent="formSubmit">
       <input type="text" v-model="textToTranslate" placeholder="Enter text to be translated">
-      <select v-model="lang">
+      <select v-model="lang" value="Select language">
         <option value="ru">Russian</option>
         <option value="fr">French</option>
         <option value="zh">Chinese</option>
@@ -23,9 +23,12 @@ export default {
       lang: '',
     };
   },
+  created() {
+    this.lang = 'ru';
+  },
   methods: {
     formSubmit() {
-      this.$emit('formSubmit', this.textToTranslate);
+      this.$emit('formSubmit', this.textToTranslate, this.lang);
     },
   },
 };
