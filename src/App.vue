efb92cd167f85c28f5858d705c8b2c6105bb1ad1
decent <danielekwuazi@gmail.com>
2017-08-29 10:29:59 +0700

    language options

--- src/App.vue
+++ src/App.vue
@@ -22,8 +22,8 @@ export default {
     TranslationOut,
   },
   methods: {
-    translateText(text) {
-      this.$http.get(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170824T042513Z.ee6ff7e10f61b641.6001d15dc0e2ce6bc258eb80692dbb5e4b5d096d&lang=ru&text=${text}`)
+    translateText(text, lang) {
+      this.$http.get(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170824T042513Z.ee6ff7e10f61b641.6001d15dc0e2ce6bc258eb80692dbb5e4b5d096d&lang=${lang}&text=${text}`)
       .then((response) => {
         this.translatedText = response.body.text[0];
       });
